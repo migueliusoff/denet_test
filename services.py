832eb6f3b574efaa568ee9683e1ef64a959d5997
Danil Galeev <galoffdan@gmail.com>
2024-10-05 14:30:03 +0300

    Add get_balance_batch function and reformat to class

--- services.py
+++ services.py
@@ -3,14 +3,29 @@ from web3 import HTTPProvider, Web3
 import settings
 
 
-def get_balance(address: str) -> int:
-    w3 = Web3(HTTPProvider("https://polygon-rpc.com"))
-    contract_address = Web3.to_checksum_address(settings.CONTRACT_ADDRESS)
-    address = Web3.to_checksum_address(address)
-    with open(settings.ABI_JSON_FILE, "r") as abi_file:
-        contract = w3.eth.contract(contract_address, abi=abi_file.read())
+class PolygonInfoService:
+    def __init__(self, contract_address: str, abi: str, http_provider_url: str = "https://polygon-rpc.com"):
+        self.w3 = Web3(HTTPProvider(http_provider_url))
+        self.contract_address = Web3.to_checksum_address(contract_address)
+        self.abi = abi
+
+    def get_balance(self, address: str) -> int:
+        address = Web3.to_checksum_address(address)
+        contract = self.w3.eth.contract(self.contract_address, abi=self.abi)
         return contract.functions.balanceOf(address).call()
+
+    def get_balance_batch(self, addresses: list[str]) -> list[int]:
+        balances = []
+        for address in addresses:
+            balances.append(self.get_balance(address))
+        return balances
 
 
 if __name__ == "__main__":
-    get_balance("0x51f1774249Fc2B0C2603542Ac6184Ae1d048351d")
+    with open(settings.ABI_JSON_FILE, "r") as abi_json_file:
+        service = PolygonInfoService(settings.CONTRACT_ADDRESS, abi_json_file.read())
+        print(
+            service.get_balance_batch(
+                ["0x51f1774249Fc2B0C2603542Ac6184Ae1d048351d", "0x4830AF4aB9cd9E381602aE50f71AE481a7727f7C"]
+            )
+        )
